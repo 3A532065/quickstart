d6e58aec503be35706fcd1028a18ffde8789460f
quickstart <weyna1649@gmail.com>
2018-10-23 18:25:32 +0800

    顯示所有任務於tasks視圖上

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,44 @@ use Illuminate\Http\Request;
 
 /**
 * 顯示我們所有任務的清單
+
 */
 
 
 Route::get('/', function () {
-    return view('welcome');
-});
+//
+$tasks = Task::orderBy('created_at', 'asc')->get();
+
+return view('tasks', [
+'tasks' => $tasks
+
+]);});
+
 
 /**
 * 增加新的任務
 */
 Route::post('/task', function (Request $request) {
 //
+// 驗證輸入
+$validator = Validator::make($request->all(), [
+'name' => 'required|max:255',
+]);
+if ($validator->fails()) {
+return redirect('/')
+->withInput()
+->withErrors($validator);
+}
+// 建立該任務...
+//新增任務存入DB的程式碼 (see next page)
+$task = new Task;
+$task->name = $request->name;
+$task->save();
+return redirect('/');
+
+
 });
+
 
 /**
 * 刪除任務
