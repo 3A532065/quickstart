277012f896eab6e7941c53c03987d0307f462738
quickstart <weyna1649@gmail.com>
2018-10-23 17:37:44 +0800

    建立三個路由

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,29 @@
-<?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Task;
+use Illuminate\Http\Request;
+
+
+/**
+* 顯示我們所有任務的清單
+*/
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+/**
+* 增加新的任務
+*/
+Route::post('/task', function (Request $request) {
+//
+});
+
+/**
+* 刪除任務
+*/
+Route::delete('/task/{task}', function (Task $task) {
+//
 });
