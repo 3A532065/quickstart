54c9658b75349b5ee383eb502ad014b82f13fe55
quickstart <weyna1649@gmail.com>
2018-10-23 18:55:46 +0800

    加入bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,16 +3,32 @@
 <head>
     <title>Laravel 快速入門 - 基本</title>
 
+
     <!-- CSS 及 JavaScript -->
+    <!-- Styles -->
+
+
 </head>
 
 <body>
 <div class="container">
     <nav class="navbar navbar-default">
         <!-- Navbar 內容 -->
+        <div class="navbar-header">
+
+            <!-- Collapsed Hamburger -->
+
+
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+        {{ config('app.name', 'Laravel') }}
+            </a>
+        </div>
     </nav>
 </div>
 
 @yield('content')
+
+
 </body>
 </html>
